f042063b76cb17863d3e6d102991465dbe2bc14e
Elham Dermaku <77150138+ElhamDe@users.noreply.github.com>
2022-10-29 23:26:24 +0200

    started

---
+++ realestate/pages/index.js
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import {Flex, Box , Text , Button} from '@chakra-ui/react'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      
+    </div>
+  )
+}
