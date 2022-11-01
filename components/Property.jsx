d397b06a5987682ee6c7a94d5be8715344a4110d
Elham Dermaku <77150138+ElhamDe@users.noreply.github.com>
2022-11-01 23:54:14 +0100

    ...

--- components/Property.jsx
+++ components/Property.jsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import { Box, Flex, Text, Avatar } from '@chakra-ui/react'
 import { FaBed, FaBath } from 'react-icons/fa'
 import { BsGridFill } from 'react-icons/bs'
-import { GoVerified } from 'react-icons/goverified'
+import { GoVerified } from 'react-icons/go'
 import millifty from 'millify'
 
 
